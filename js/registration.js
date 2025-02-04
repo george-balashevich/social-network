ca0c5df1ca74e73f29d25a1dcd39549fc58653d0
George <george@MacBook-Pro-George.local>
2025-02-04 19:43:38 +0100

    added validation of registration page

--- js/registration.js
+++ js/registration.js
@@ -1,1 +1,9 @@
-const inputLogin = document.querySelector
+import { submitForm } from './submitForm.js'
+
+function registration() {
+  submitForm('registrationForm', 'registrationBtn')
+}
+
+registration()
+
+export { registration }
---
+++ js/utils/checkPassword.js
@@ -0,0 +1,17 @@
+function checkBothPasswords(passwordSelector, repeatedPasswordSelector, errorSelector) {
+  const password = document.querySelector(passwordSelector).value.trim()
+  const repeatedPassword = document.querySelector(repeatedPasswordSelector).value.trim()
+  const errorText = document.querySelector(errorSelector)
+
+  if (!errorText) return false
+
+  if (password === repeatedPassword) {
+    errorText.innerText = ''
+    return true
+  } else {
+    errorText.innerText = 'Пароли не совпадают'
+    return false
+  }
+}
+
+export { checkBothPasswords }
